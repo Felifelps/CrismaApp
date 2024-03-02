4af0b75ebf99dd179f3893620bfe2516555a2849
Felifelps <felipefelipe23456@gmail.com>
2024-03-02 12:41:43 -0300

    Added auto_request

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import threading
+import time
+
+import requests
+
 from crismaapp import app
+
+def auto_request():
+    while True:
+        print('REQUESTING')
+        requests.get('https://crismafronteiro.onrender.com', timeout=300)
+        time.sleep(60)
+
+threading.Thread(target=auto_request, daemon=True).start()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)
