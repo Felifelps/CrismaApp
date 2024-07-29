2d84019d8cf9fc39a53a497d44ccd95d9ce145cd
Felifelps <felipefelipe23456@gmail.com>
2024-07-29 15:27:44 -0300

    render deploy fails

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from crismaapp import app
 background.worker.start()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=False, port=8080)
