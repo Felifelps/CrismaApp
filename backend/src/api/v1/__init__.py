e14c868b9c937c86abfed3ff8a6d8b110851a437
Felifelps <felipefelipe23456@gmail.com>
2024-08-13 09:11:57 -0300

    Added frequency checking

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -6,8 +6,10 @@ from .auth import auth
 from .crismandos import crismandos
 from .domingos import domingos
 from .encontros import encontros
+from .frequency import frequency
 
 v1.register_blueprint(auth, url_prefix='/auth')
 v1.register_blueprint(crismandos, url_prefix='/crismandos')
 v1.register_blueprint(domingos, url_prefix='/domingos')
 v1.register_blueprint(encontros, url_prefix='/encontros')
+v1.register_blueprint(frequency, url_prefix='/frequency')
---
+++ backend/src/api/v1/frequency.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, jsonify
+from src.models import Crismando, FrequenciaDomingo, FrequenciaEncontro
+from src.utils import model_to_dict
+
+
+frequency = Blueprint('frequency', __name__)
+
+@frequency.route('/')
+def get_frequency():
+    search = request.get_json().get('search')
+    if not search:
+        return jsonify(error='Missing search'), 400
+
+    for crismando in Crismando.select():
+        if crismando.nome.lower() == search.lower():
+            return {
+                'frequenciaencontro': {f.id: model_to_dict(f) for f in FrequenciaEncontro.filter(crismando=crismando)},
+                'frequenciadomingo': {f.id: model_to_dict(f) for f in FrequenciaDomingo.filter(crismando=crismando)},
+            }
+    
+    return jsonify(message='Name not found'), 404
+    
